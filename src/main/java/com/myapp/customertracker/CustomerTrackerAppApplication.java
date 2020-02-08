03343f0ed2c092f221c2ce0ad4cb733748bb0cee
grdevv@gmail.com <grdevv@gmail.com>
2020-02-08 13:45:14 +0200

    added data.sql to populate the DB

--- src/main/java/com/myapp/customertracker/CustomerTrackerAppApplication.java
+++ src/main/java/com/myapp/customertracker/CustomerTrackerAppApplication.java
@@ -3,11 +3,9 @@ package com.myapp.customertracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ServletComponentScan
-@EnableAspectJAutoProxy
 public class CustomerTrackerAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+DELETE FROM `customer`;
+
+INSERT INTO `customer` VALUES
+	(1,'David','Adams','david@gmail.com'),
+	(2,'John','Doe','john@gmail.com'),
+	(3,'Ajay','Rao','ajay@gmail.com'),
+	(4,'Mary','Public','mary@gmail.com'),
+	(5,'Maxwell','Dixon','max@gmail.com');
